bd183e85e28a7e4c4f7e3f81749b71b0d7121cd5
Bozidar Spasenovic <spasenovic2002@gmail.com>
2018-09-18 09:46:28 +0200

    First version

---
+++ hello_world.c
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2ahif
+ * ---------------------------------------------------------
+ * Exercise Number: 22
+ * Title:			-
+ * Author:			Bozidar Spasenovic
+ * ----------------------------------------------------------
+ * Description:
+ * 
+ * ----------------------------------------------------------
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  printf("Hello World\n");
+  return 0;
+}
